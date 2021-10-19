9e1a3bedc5b20f971e1aca842946faa75a47c11f
Magdalena <perezmagdalenaines@gmail.com>
2021-10-18 23:17:12 -0300

    agregué el array de productos

---
+++ js/productos.js
@@ -0,0 +1,108 @@
+const productos = [
+    { 
+        "imagen": "images/cart-img-brocoli.png",
+        "imagenCart": "images/brocoli.jpg",
+        "nombre": "Brocoli x planta", 
+        "precio": "$180", 
+        "id": 1
+    },                
+    {  
+        "imagen": "images/cart-img-cebolla.png",
+        "imagenCart": "images/cebolla.jpg",
+        "nombre": "Cebolla x kg", 
+        "precio": "$99", 
+        "id": 2
+    },
+    {  
+        "imagen": "images/cart-img-kale.png",
+        "imagenCart": "images/kale.jpg",
+        "nombre": "Kale x atado", 
+        "precio": "$165", 
+        "id": 3
+    },
+    {  
+        "imagen": "images/cart-img-lechuga.png",
+        "imagenCart": "images/lechuga.jpg",
+        "nombre": "Lechuga x planta", 
+        "precio": "$99", 
+        "id": 4
+    },
+    {  
+        "imagen": "images/cart-img-palta.png",
+        "imagenCart": "images/palta.jpg",
+        "nombre": "Palta x unidad", 
+        "precio": "$125", 
+        "id": 5
+    },
+    {  
+        "imagen": "images/cart-img-papa.png",
+        "imagenCart": "images/papa.jpg",
+        "nombre": "Papa x kg", 
+        "precio": "$115", 
+        "id": 6
+    },
+    {  
+        "imagen": "images/cart-img-morron.png",
+        "imagenCart": "images/morron.jpg",
+        "nombre": "Morrón x unidad", 
+        "precio": "$120", 
+        "cantidad": "x unidad",
+        "id": 7
+    },
+    {  
+        "imagen": "images/cart-img-tomate.png",
+        "imagenCart": "images/tomate.jpg",
+        "nombre": "Tomate x kg", 
+        "precio": "$145", 
+        "id": 8
+    },
+    {  
+        "imagen": "images/cart-img-zanahoria.png",
+        "imagenCart": "images/zanahoria.jfif",
+        "nombre": "Zanahoria x kg", 
+        "precio": "$22", 
+        "id": 9
+    },
+    {  
+        "imagen": "images/cart-img-banana.png",
+        "imagenCart": "images/banana.jpg",
+        "nombre": "Banana x kg", 
+        "precio": "$155", 
+        "id": 10
+    },
+    {  
+        "imagen": "images/cart-img-frutillas.png",
+        "imagenCart": "images/frutilla.jpg",
+        "nombre": "Frutillas x kg", 
+        "precio": "$450", 
+        "id": 11
+    },
+    {  
+        "imagen": "images/cart-img-limon.png",
+        "imagenCart": "images/limon.jpg",
+        "nombre": "Limon x kg", 
+        "precio": "$170", 
+        "id": 12
+    },
+    {  
+        "imagen": "images/cart-img-mandarina.png",
+        "imagenCart": "images/mandarina.jpg",
+        "nombre": "Mandarina x kg", 
+        "precio": "$79", 
+        "id": 13
+    },
+    {  
+        "imagen": "images/cart-img-manzana.png",
+        "imagenCart": "images/manzana.jpg",
+        "nombre": "Manzana x kg", 
+        "precio": "$160", 
+        "id": 14
+    },
+    {  
+        "imagen": "images/cart-img-naranja.png",
+        "imagenCart": "images/naranja.jpg",
+        "nombre": "Naranja x kg", 
+        "precio": "$150", 
+        "id": 15
+    }
+];
